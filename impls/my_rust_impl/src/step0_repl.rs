bd323243c24c4d992c5681ae78bb2433dcdcbcb8
skliew <skliew@gmail.com>
2021-07-06 01:04:31 +0800

    First commit

---
+++ impls/my_rust_impl/src/step0_repl.rs
@@ -0,0 +1,26 @@
+
+fn read(input: String) -> String {
+    input
+}
+
+fn eval(input: String) -> String {
+    input
+}
+
+fn print(input: String) -> String {
+    input
+}
+
+fn rep(input: String) -> String {
+    print(eval(read(input)))
+}
+
+fn main() -> Result<(), std::io::Error> {
+    loop {
+        let mut input = String::new();
+        println!("user> ");
+        std::io::stdin().read_line(&mut input)?;
+        rep(input);
+    }
+}
+
